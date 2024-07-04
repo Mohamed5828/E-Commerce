d98f34482048469db25bacdd36c0a276b9e55952
mohamed <mohamed5828@github.com>
2024-07-04 09:28:15 +0300

    first proto for single page

--- utils/FetchData.js
+++ utils/FetchData.js
@@ -1,2 +1,24 @@
-//function to expect the url and JWT or any other identifying paramters
-//should return data , isLoading, Error
+// FetchData.js
+export async function fetchData(url) {
+  const result = {
+    data: null,
+    isLoading: true,
+    isError: false,
+  };
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    result.data = data;
+  } catch (error) {
+    result.isError = true;
+    console.error("Error fetching data:", error);
+  } finally {
+    result.isLoading = false;
+  }
+
+  return result;
+}
