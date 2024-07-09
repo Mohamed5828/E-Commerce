efc77ab6e9e82862fa40635d556cbd6f7b3efc8a
mohamed <mohamed5828@github.com>
2024-07-09 11:04:56 +0300

    merging search

--- utils/SearchBar.js
+++ utils/SearchBar.js
@@ -1,67 +1,61 @@
-<<<<<<< HEAD
-=======
 //Searching
->>>>>>> 0ec206c4c21ee06b1a88d8b029f490016c1fb27d
-document.getElementById('searchIcon').addEventListener('click', function() {
-    let searchInput = document.getElementById('newSearchInput');
-    if (searchInput.classList.contains('show')) {
-        searchInput.classList.remove('show');
-    } else {
-        searchInput.classList.add('show');
-        searchInput.focus();
-    }
+document.getElementById("searchIcon").addEventListener("click", function () {
+  let searchInput = document.getElementById("newSearchInput");
+  if (searchInput.classList.contains("show")) {
+    searchInput.classList.remove("show");
+  } else {
+    searchInput.classList.add("show");
+    searchInput.focus();
+  }
 });
-document.getElementById('newSearchInput').addEventListener('blur', function() {
-    let searchInput = document.getElementById('newSearchInput');
-    searchInput.classList.remove('show');
+document.getElementById("newSearchInput").addEventListener("blur", function () {
+  let searchInput = document.getElementById("newSearchInput");
+  searchInput.classList.remove("show");
 });
 
-document.getElementById('newSearchInput').addEventListener('keyup',newsearchProducts)
+document
+  .getElementById("newSearchInput")
+  .addEventListener("keyup", newsearchProducts);
 
-function newsearchProducts(){
-    const query=document.getElementById('newSearchInput').value;
-    fetch(`https://dummyjson.com/products/search?q=${query}`)
-        .then(res => res.json())
-        .then(data => {
-            displayProducts(data.products);
-        })
-        .catch(error => {
-            console.error('Error fetching products:', error);
-        });
+function newsearchProducts() {
+  const query = document.getElementById("newSearchInput").value;
+  fetch(`https://dummyjson.com/products/search?q=${query}`)
+    .then((res) => res.json())
+    .then((data) => {
+      displayProducts(data.products);
+    })
+    .catch((error) => {
+      console.error("Error fetching products:", error);
+    });
 }
-
-<<<<<<< HEAD
-=======
 
 //Categorization
 const dropdownToggle = document.getElementById("dropdowntoggle");
-const dropdownMenu = document.getElementById('dropdownmenu');
+const dropdownMenu = document.getElementById("dropdownmenu");
 
 function categorizeProducts() {
-    const category = document.getElementById('category').value;
+  const category = document.getElementById("category").value;
 
-    fetch(`https://dummyjson.com/products/category/${category}`)
-        .then(res => res.json())
-        .then(data => displayProducts(data.products))
-        .catch(error => console.error('Error fetching products:', error));
+  fetch(`https://dummyjson.com/products/category/${category}`)
+    .then((res) => res.json())
+    .then((data) => displayProducts(data.products))
+    .catch((error) => console.error("Error fetching products:", error));
 }
 
-
 //display function
->>>>>>> 0ec206c4c21ee06b1a88d8b029f490016c1fb27d
-function displayProducts(products){
-    const productList = document.getElementById('product-list');
-    productList.innerHTML= '';
+function displayProducts(products) {
+  const productList = document.getElementById("product-list");
+  productList.innerHTML = "";
 
-    products.forEach(product => {
-        const productDiv=document.createElement('div');
-        productDiv.className='product'
-        productDiv.innerHTML = `
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.className = "product";
+    productDiv.innerHTML = `
             <img src="${product.thumbnail}">
             <h2>${product.title}</h2>
             <p>${product.description}</p><br>
             <p>Price: $${product.price}</p>
         `;
-        productList.appendChild(productDiv);
-    });
+    productList.appendChild(productDiv);
+  });
 }
