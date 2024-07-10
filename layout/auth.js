e1a3503ea81b8acd5b7502d0d4ef285199fbf935
Ahmed R. El-Kilani <ahmedelkilani01@gmail.com>
2024-07-10 11:57:34 +0300

    added profile with place holder info | WIP fetch userdata into profile

--- layout/auth.js
+++ layout/auth.js
@@ -35,14 +35,6 @@ if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark-mode");
   }
 
-
-// Uncomment the following code to use placeholder users
-// localStorage.setItem("auth-users", `[
-// {"id":0,"name":"John Doe","email":"johndoe@example.com","salt":"85b8900359bea6e3bbc948525a2f6ece","hashed_password":"9284fee6a249dae70848c8287be857612661ef6fa09908a390ceafeb8b80bee6"},
-// {"id":1,"name":"Jane Smith","email":"janesmith@example.com","salt":"93b0b8dc2cdeb5e68b89a1f6155c618c","hashed_password":"ae92a16cfb2e7372ae6290f86e58c98d85d1ea4d46bd11cd76da46bc399345d1"},
-// {"id":2,"name":"Michael Brown","email":"michaelbrown@example.com","salt":"f1793f9363496cc76b0d0e743e803ec8","hashed_password":"0810ed6d4faa7ff952044ccb1de2f2e59dd76df046b5ffa8eb5853ad6c7f2e7f"}
-// ]`);
-// localStorage.setItem("auth-user-id-counter", "3");
 //_______________________________________________________
 // LEAVE COMMENTED
 // =================
