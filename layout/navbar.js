9f49817abaaf1b83985086d96e9b570667a212e9
KhaledHamouda <khaled7amoda@gmail.com>
2024-07-13 22:26:17 +0300

    Fixing navbar bugs.

--- layout/navbar.js
+++ layout/navbar.js
@@ -1,18 +1,3 @@
-// //navbar samll
-// const navList = document.querySelector(".nav-list");
-
-// document.querySelector(".hamburger").onclick = () => {
-//   navList.classList.add("show");
-// };
-
-// document.querySelector(".close").onclick = () => {
-//   navList.classList.remove("show");
-// };
-
-// const body = document.body;
-// if (localStorage.getItem("theme") === "dark") {
-//   body.classList.add("dark-mode");
-// }
 import { renderData } from "./products.js";
 
 const navbarContainer = document.getElementById("navbar");
