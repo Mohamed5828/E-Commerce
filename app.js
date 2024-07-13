af071fc34bdcae8e39485b8513e002665efb30c7
mohamed5828 <mohamed.ahmed5828@gmail.com>
2024-07-13 17:12:12 +0300

    edit btns

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggleButton = document.getElementById("theme-toggle");
-  const body = document.body;
+// document.addEventListener("DOMContentLoaded", () => {
+//   const toggleButton = document.getElementById("theme-btn");
+//   const body = document.body;
 
-  toggleButton.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
+//   toggleButton.addEventListener("click", () => {
+//     body.classList.toggle("dark-mode");
 
-    // Save the style in local storage
-    if (body.classList.contains("dark-mode")) {
-      localStorage.setItem("theme", "dark");
-    } else {
-      localStorage.setItem("theme", "light");
-    }
-  });
+//     // Save the style in local storage
+//     if (body.classList.contains("dark-mode")) {
+//       localStorage.setItem("theme", "dark");
+//     } else {
+//       localStorage.setItem("theme", "light");
+//     }
+//   });
 
-  // Load the style preference on page load
-  if (localStorage.getItem("theme") === "dark") {
-    body.classList.add("dark-mode");
-  }
-});
+//   // Load the style preference on page load
+//   if (localStorage.getItem("theme") === "dark") {
+//     body.classList.add("dark-mode");
+//   }
+// });
